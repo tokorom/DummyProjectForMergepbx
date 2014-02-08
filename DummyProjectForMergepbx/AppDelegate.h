937797464a0416cc06ade5a3ea7b4fcda8b146cb
tokorom <tokorom@gmail.com>
2014-02-08 09:11:39 +0900

    First commit

---
+++ DummyProjectForMergepbx/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DummyProjectForMergepbx
+//
+//  Created by ytokoro on 2/8/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DummyProjectForMergepbx/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DummyProjectForMergepbx
+//
+//  Created by ytokoro on 2/8/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
